1a74edbfe070d4174ce1ff10f20c18f71d0a8dc4
Marina Sergeyeva <morrrinad@gmail.com>
2020-08-18 17:10:16 +0300

    task-01 finished

--- src/Components/App.js
+++ src/Components/App.js
@@ -10,65 +10,92 @@ export default class App extends Component {
       {
         id: "id-1",
         name: "Rosie Simpson",
-        number: "459-12-56"
+        number: "459-12-56",
       },
       {
         id: "id-2",
         name: "Hermione Kline",
-        number: "443-89-12"
+        number: "443-89-12",
       },
       {
         id: "id-3",
         name: "Eden Clements",
-        number: "645-17-79"
+        number: "645-17-79",
       },
       {
         id: "id-4",
         name: "Annie Copeland",
-        number: "227-91-26"
-      }
+        number: "227-91-26",
+      },
     ],
-    filter: ""
+    filter: "",
   };
+
+  componentDidMount() {
+    console.log("Task componentDidMount");
+    const persistedContacts = localStorage.getItem("contacts");
+
+    if (persistedContacts) {
+      this.setState({
+        contacts: JSON.parse(persistedContacts),
+      });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log("Task componentDidUpdate");
+
+    if (prevProps !== prevState) {
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+    }
+  }
 
   addItem = ({ name, number }) => {
     const { contacts } = this.state;
 
-    if (contacts.find(contact => contact.name.toLowerCase() === name.toLowerCase())) {
+    if (
+      contacts.find(
+        (contact) => contact.name.toLowerCase() === name.toLowerCase()
+      )
+    ) {
       alert("The name is already exsist");
     } else {
-      this.setState(prevState => ({
+      this.setState((prevState) => ({
         contacts: [
           ...prevState.contacts,
           {
             id: uuidv4(),
             name,
-            number
-          }
-        ]
+            number,
+          },
+        ],
       }));
     }
   };
 
-  getFilter = e => {
+  getFilter = (e) => {
     console.log(e.target);
     const { value } = e.target;
 
     this.setState({
-      filter: value
+      filter: value,
     });
   };
 
   findContact = () => {
     const { contacts, filter } = this.state;
     console.log(contacts);
-    return filter ? contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase())) : contacts;
+    return filter
+      ? contacts.filter((contact) =>
+          contact.name.toLowerCase().includes(filter.toLowerCase())
+        )
+      : contacts;
   };
 
-  deleteContact = id => {
-    this.setState(prevState => {
+  deleteContact = (id) => {
+    this.setState((prevState) => {
       return {
-        contacts: prevState.contacts.filter(contact => contact.id !== id)
+        contacts: prevState.contacts.filter((contact) => contact.id !== id),
       };
     });
   };
@@ -81,7 +108,10 @@ export default class App extends Component {
         <ContactForm addItem={this.addItem} />
         <p className="sectionTitle"> Contacts </p>
         <Filter filter={filter} getFilter={this.getFilter} />
-        <ContactsList contacts={this.findContact()} deleteContact={this.deleteContact} />
+        <ContactsList
+          contacts={this.findContact()}
+          deleteContact={this.deleteContact}
+        />
       </>
     );
   }
